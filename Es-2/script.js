da3bb6688ee4369774f30a02eb2d0c4cdd169c53
NoeFabris <noe@elysresidence.com>
2021-04-29 15:50:42 +0200

    Fix

--- Es-2/script.js
+++ Es-2/script.js
@@ -6,21 +6,25 @@ for (let i = 1; i < 11; i++) {
     var zucchina = {}
     zucchina['tipo'] = 'tipo' + (i),
     zucchina['peso'] = numGenDecimal(0, 3),
-    zucchina['lunghezza'] = numGenDecimal(0, 25) + 'cm'
+    zucchina['lunghezza'] = numGenDecimal(0, 25)
     zucchine.push(zucchina)
 }
 
 var zucchineCorte = []
+var zucchineCortePeso = 0
 var zucchineLunghe = []
+var zucchineLunghePeso = 0
 
 for (let k = 0; k < zucchine.length; k++) {
-    var lunghezzaZucchina = parseFloat(zucchine[k]['lunghezza'])
-    // console.log(lunghezzaZucchina)
-    if (lunghezzaZucchina > 15) {
+    var lunghezzaZucchina = zucchine[k]
+    console.log(lunghezzaZucchina)
+    if (lunghezzaZucchina['lunghezza'] > 15) {
         zucchineLunghe.push(lunghezzaZucchina)
+        zucchineLunghePeso += parseFloat(lunghezzaZucchina['peso']) 
     } else {
         zucchineCorte.push(lunghezzaZucchina)
-    }
+        zucchineCortePeso += parseFloat(lunghezzaZucchina['peso'])
+    } 
 }
-console.log('Numero di zucchine piu corte di 15cm ' +  zucchineCorte.length)
-console.log('Numero di zucchine piu lunghe di 15cm ' +  zucchineLunghe.length)
+console.log(`Numero di zucchine piu corte di 15cm ${zucchineCorte.length} ed il loro peso è: ${zucchineCortePeso}`)
+console.log(`Numero di zucchine piu corte di 15cm ${zucchineLunghe.length} ed il loro peso è: ${zucchineLunghePeso}`)
